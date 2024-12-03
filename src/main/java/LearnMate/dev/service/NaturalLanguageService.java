4196c894d38694ef15057c6f186679320be3faf7
daeun084 <030804jk@naver.com>
2024-12-03 19:24:31 +0900

    !hotfix: log error message

--- src/main/java/LearnMate/dev/service/NaturalLanguageService.java
+++ src/main/java/LearnMate/dev/service/NaturalLanguageService.java
@@ -5,12 +5,14 @@ import LearnMate.dev.common.ErrorStatus;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Service
 public class NaturalLanguageService {
 
@@ -31,6 +33,7 @@ public class NaturalLanguageService {
 
             return CompletableFuture.completedFuture(sentiment.getScore());
         } catch (IOException e) {
+            log.error(e.getMessage());
             throw new ApiException(ErrorStatus._ANALYZE_EMOTION_ERROR);
         }
     }
