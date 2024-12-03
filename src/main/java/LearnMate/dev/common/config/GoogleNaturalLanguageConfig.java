0ae65ff0da1c25af1995232387af019558fb536d
daeun084 <030804jk@naver.com>
2024-12-03 19:32:35 +0900

    !hotfix: gcp credentials config

--- src/main/java/LearnMate/dev/common/config/GoogleNaturalLanguageConfig.java
+++ src/main/java/LearnMate/dev/common/config/GoogleNaturalLanguageConfig.java
@@ -2,6 +2,7 @@ package LearnMate.dev.common.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.language.v1.LanguageServiceSettings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,17 +10,19 @@ import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class GoogleNaturalLanguageConfig {
-    @Value("classpath:nlp.json")
-    Resource gcsCredentials;
+    @Value("${GCP_CREDENTIALS_LOCATION}")
+    Resource gcpCredentials;
 
     @Bean
     public LanguageServiceSettings languageServiceSettings() {
         try {
+            log.info("Loading Google Natural Language Service");
             return LanguageServiceSettings.newBuilder()
                     .setCredentialsProvider(() ->
-                            GoogleCredentials.fromStream(gcsCredentials.getInputStream()))
+                            GoogleCredentials.fromStream(gcpCredentials.getInputStream()))
                     .build();
         } catch (IOException e) {
             throw new RuntimeException("Failed to initialize LanguageServiceSettings", e);
